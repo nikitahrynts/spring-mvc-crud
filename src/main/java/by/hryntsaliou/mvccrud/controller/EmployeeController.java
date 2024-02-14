6473dfcd43727aa0740a9cb99f31755eab59c7c0
Nikita <hrynts2002@gmail.com>
2024-02-15 01:32:24 +0300

    Added Get Mapping and thymeleaf template to represent data

---
+++ src/main/java/by/hryntsaliou/mvccrud/controller/EmployeeController.java
@@ -0,0 +1,33 @@
+package by.hryntsaliou.mvccrud.controller;
+
+import by.hryntsaliou.mvccrud.entity.Employee;
+import by.hryntsaliou.mvccrud.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model model) {
+
+        List<Employee> employees = employeeService.findAll();
+        model.addAttribute("employees", employees);
+
+        return "list-employees";
+    }
+}
